091d13ccc65e66710faf99a7c699703b1d8109db
Yedda <YeddaChuang@gmail.com>
2018-06-12 00:29:49 +0800

    setup category resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :restaurants, only: [:index, :show]
   root "restaurants#index"
+
+  resources :categories, only: [:show]
 
   namespace :admin do
     resources :restaurants
