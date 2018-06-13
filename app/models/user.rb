3defd82267e19389d0eb1a0ad96d6d49ee6638fd
Yedda <YeddaChuang@gmail.com>
2018-06-13 22:06:58 +0800

    add uploader for user to upload avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
+  mount_uploader :image, AvatarUploader
   def admin?
     self.role == "admin"
   end
