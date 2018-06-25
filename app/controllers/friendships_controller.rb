2a97c7152b300c47e180f86a0aff8cb9110ab5d0
Yedda <YeddaChuang@gmail.com>
2018-06-26 01:29:53 +0800

    add create/destroy function in friendshipController

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,24 @@
 class FriendshipsController < ApplicationController
+  def create
+    #@friendship = current_user.friends.build(friend_id: params[:friend_id])
+    @friendship = Friendship.create(friend_id: params[:friend_id], user_id: current_user.id, status: 1)
+
+    if @friendship.save
+      flash[:notice] = "Successfully add friend"
+    else
+      flash[:alert] = @friendship.errors.full_messages.to_sentence
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @friendship = current_user.friendships.where(friend_id: params[:id]).first
+    if @friendship
+      @friendship.destroy
+      flash[:alert] = "Friendship destroyed"
+    else
+      flash[:alert] = "No friendship"
+    end
+    redirect_back(fallback_location: root_path)
+  end
 end
